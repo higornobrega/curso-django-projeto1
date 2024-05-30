f61e5ff81664f0aeb65421c6e8f58a8e2d4f5422
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-29 21:50:44 -0300

    Feature - Python-dotenv- moving PER_PAGE to variable in environment in Django (.env)

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -5,12 +7,13 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from recipes.models import Recipe
 from utils.pagination import make_pagination, make_pagination_range
 
+PER_PAGE = os.environ.get('PER_PAGE', 6)
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -25,7 +28,7 @@ def category(request, category_id):
             is_published=True,
         ).order_by('-id')
     )
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -58,7 +61,7 @@ def search(request):
         is_published=True,
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     return render(request, 'recipes/pages/search.html',{
         'page_title':f'Search for "{search_term}"',
         'search_term':search_term,
