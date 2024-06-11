d33c3a8e21c8eb99ee016ee174c59f308ea0613a
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-06-10 21:52:05 -0300

    Feature - Creating a form for the user to register using the default User  model

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 
 # Create your views here.
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form':form,
+    }
+    return render(request, 'authors/pages/register_view.html', context)
