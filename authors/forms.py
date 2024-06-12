9d70ffb4882449e26edfc5a632b7c1a65450274a
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-06-11 21:27:06 -0300

    Feature - Sending Post to another URL and using SESSION to save values between requests

--- authors/forms.py
+++ authors/forms.py
@@ -11,4 +11,4 @@ class RegisterForm(forms.ModelForm):
             'username',
             'email',
             'password',
-            ]
+        ]
--- authors/urls.py
+++ authors/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register_view
+from . import views
 
 app_name = 'authors'
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='create'),
 ]
