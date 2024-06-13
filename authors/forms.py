c4e58fcd8bd26e283f19231f20502ec7b6b0d319
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-06-12 21:39:26 -0300

    Feature - Configuring the form with labels, widgets, erros, field attributes, etc

--- authors/forms.py
+++ authors/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
+        # Campos a serem exibidos
         fields = [
             'first_name',
             'last_name',
@@ -12,3 +13,38 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        
+        # Campos a serem excluídos
+        # exclude = ['first_name'] 
+        
+        # Titulo que mostra a cima do campo 
+        labels = {
+            'first_name': 'first_name',
+            'last_name': 'last_name',
+            'username': 'username',
+            'email': 'email',
+            'password': 'password',
+        }
+        
+        # Mensagem de ajuda abaixo do campo
+        help_texts = {
+            'email':'The e-mail must be valid',
+        }
+        
+        # Mensagem de erro
+        error_messages = {
+            'username':{
+                'required': 'This field must not be empty', # Código do erro : mensagem do erro
+            }
+        }
+        
+        # Tipo e atributos para campo
+        widgets = {
+            'first_name':forms.TextInput(attrs={
+                'placeholder':'Type your username here',
+                'class': 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                    'placeholder': 'Type your password here'
+            })
+        }
